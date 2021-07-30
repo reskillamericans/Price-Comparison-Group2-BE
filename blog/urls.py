2b196e018a90acb6e13ed9c660e72c2d13e3342a
Valli-dev <vallimayilannamalai@gmail.com>
2021-07-29 23:37:38 -0700

    Fixes with blog app models and Item.html added

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .import views 
-from .views import like_view, get_product,details
+from .views import like_view, get_product,details, compare_price
 urlpatterns = [
-        #path('', views.homeview, name='home'),
+        
         path('', get_product, name='homeview'),
         path('details/<int:id>',details,name='product-detailsview'),
         path('like/',like_view, name='like_product'),
+        path('compare_price/', compare_price, name='compare_price'),
 
 
 
--- price_comparison_gp2/urls.py
+++ price_comparison_gp2/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings
 from products.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('accounts/', include('accounts.urls')),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('blog/',include('blog.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
